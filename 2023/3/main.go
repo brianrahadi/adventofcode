a2306844a49b94f8c348d1920c1ff0a1dd26d0f9
Brian Rahadi <brian.rahadi@gmail.com>
2024-12-02 14:54:01 -0800

    finish 2023 3 with baddd code

--- 2023/3/main.go
+++ 2023/3/main.go
@@ -83,7 +83,6 @@ func part1(lines []string) {
 			if unicode.IsDigit(matrix[r][c]) && isSymbolAdjacent(matrix, r, c) {
 				num, key := getNumber(matrix, r, c)
 				if !coordinateSet[key] {
-					// fmt.Println(num)
 					sum += num
 				}
 				coordinateSet[key] = true
@@ -95,12 +94,68 @@ func part1(lines []string) {
 	fmt.Println(sum)
 }
 
-func part2(lines []string) {
-	for _, line := range lines {
-		_ = line
+func isSymbolAdjacentWithKey(matrix [][]rune, r int, c int) (bool, string) {
+	adj := [8][2]int{
+		{0, 1},
+		{1, 1},
+		{1, 0},
+		{1, -1},
+		{0, -1},
+		{-1, -1},
+		{-1, 0},
+		{-1, 1},
 	}
 
-	fmt.Println("answer2")
+	for _, val := range adj {
+		ax, ay := r+val[0], c+val[1]
+
+		if ax < 0 || ax >= len(matrix) || ay < 0 || ay >= len(matrix) {
+			continue
+		}
+
+		if !unicode.IsDigit(matrix[ax][ay]) && matrix[ax][ay] != '.' {
+			return true, fmt.Sprintf("%d,%d", ax, ay)
+		}
+	}
+	return false, ""
+}
+
+func part2(lines []string) {
+	matrix := [][]rune{}
+	for _, line := range lines {
+		splittedLine := strings.Split(line, "")
+		runes := lo.Map(splittedLine, func(s string, _ int) rune { return rune(s[0]) })
+		matrix = append(matrix, runes)
+	}
+
+	sum := 0
+	coordinateSet := map[string]bool{}
+	symbolKeySet := map[string]int{}
+	symbolKeyCount := map[string]int{}
+
+	for r := range matrix {
+		for c := range matrix[r] {
+			if !unicode.IsDigit(matrix[r][c]) {
+				continue
+			}
+			isAdj, symbolKey := isSymbolAdjacentWithKey(matrix, r, c)
+			if isAdj {
+				num, key := getNumber(matrix, r, c)
+				if symbolKeySet[symbolKey] > 0 && symbolKeyCount[symbolKey] == 1 {
+					if coordinateSet[key] {
+						continue
+					}
+					sum += num * symbolKeySet[symbolKey]
+				}
+				symbolKeyCount[symbolKey] += 1
+				symbolKeySet[symbolKey] = num
+				coordinateSet[key] = true
+			}
+
+		}
+	}
+
+	fmt.Println(sum)
 }
 
 func main() {
